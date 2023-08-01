3f175a436eae7d76bd1cb9c408ec47baebd36525
Nolan Ollada <nool0624@colorado.edu>
2023-08-01 04:39:16 +0000

    add db_create

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn = psycopg2.connect("postgres://btdb_user:ALZcuvXRdp1JTbE8uq3WMQmCZMzGoc0y@dpg-cj4766l9aq047cak6930-a/btdb")
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgres://btdb_user:ALZcuvXRdp1JTbE8uq3WMQmCZMzGoc0y@dpg-cj4766l9aq047cak6930-a/btdb")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Successfully Created'
